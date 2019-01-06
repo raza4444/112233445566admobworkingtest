bd6af56cd29577c6de92ef6b8371076a347681ac
appgenny <appgenerator2@gmail.com>
2019-01-06 16:59:31 +0500

    asdasd

--- plugins/cordova-admob-plus/src/android/ads/RewardedVideoAd.java
+++ plugins/cordova-admob-plus/src/android/ads/RewardedVideoAd.java
@@ -21,15 +21,15 @@ public class RewardedVideoAd extends AdBase {
     }
 
     public static boolean executeIsReadyAction(Action action, CallbackContext callbackContext) {
-        // plugin.cordova.getActivity().runOnUiThread(new Runnable() {
-        //     @Override
-        //     public void run() {
-        //         RewardedVideoAd rewardedVideoAd = (RewardedVideoAd) action.getAd();
+        /*plugin.cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                RewardedVideoAd rewardedVideoAd = (RewardedVideoAd) action.getAd();
 
-        //         PluginResult result = new PluginResult(PluginResult.Status.OK, rewardedVideoAd != null && rewardedVideoAd.isReady());
-        //         callbackContext.sendPluginResult(result);
-        //     }
-        // });
+                PluginResult result = new PluginResult(PluginResult.Status.OK, rewardedVideoAd != null && rewardedVideoAd.isReady());
+                callbackContext.sendPluginResult(result);
+            }
+        });*/
 
         return true;
     }
