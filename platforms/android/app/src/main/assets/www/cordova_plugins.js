0c827570cf2f4addb6351c2c887ba5e43b3b8df5
appgenny <appgenerator2@gmail.com>
2019-01-06 15:12:18 +0500

    asdasdasd

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -31,6 +31,39 @@ module.exports = [
     "clobbers": [
       "window.StatusBar"
     ]
+  },
+  {
+    "id": "cordova-plugin-network-information.network",
+    "file": "plugins/cordova-plugin-network-information/www/network.js",
+    "pluginId": "cordova-plugin-network-information",
+    "clobbers": [
+      "navigator.connection",
+      "navigator.network.connection"
+    ]
+  },
+  {
+    "id": "cordova-plugin-network-information.Connection",
+    "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+    "pluginId": "cordova-plugin-network-information",
+    "clobbers": [
+      "Connection"
+    ]
+  },
+  {
+    "id": "cordova-plugin-customconfigparameters.CustomConfigParameters",
+    "file": "plugins/cordova-plugin-customconfigparameters/www/customconfigparameters.js",
+    "pluginId": "cordova-plugin-customconfigparameters",
+    "clobbers": [
+      "CustomConfigParameters"
+    ]
+  },
+  {
+    "id": "cordova-plugin-appversion.RareloopAppVersion",
+    "file": "plugins/cordova-plugin-appversion/www/app-version.js",
+    "pluginId": "cordova-plugin-appversion",
+    "clobbers": [
+      "AppVersion"
+    ]
   }
 ];
 module.exports.metadata = 
@@ -40,7 +73,10 @@ module.exports.metadata =
   "cordova-admob-plus": "0.0.0",
   "onesignal-cordova-plugin": "2.4.6",
   "cordova-plugin-splashscreen": "5.0.2",
-  "cordova-plugin-statusbar": "2.4.2"
+  "cordova-plugin-statusbar": "2.4.2",
+  "cordova-plugin-network-information": "2.0.1",
+  "cordova-plugin-customconfigparameters": "3.0.0",
+  "cordova-plugin-appversion": "1.0.0"
 };
 // BOTTOM OF METADATA
 });
--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -49,6 +49,45 @@ module.exports = [
         "id": "cordova-plugin-statusbar.StatusBarProxy",
         "pluginId": "cordova-plugin-statusbar",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/src/browser/network.js",
+        "id": "cordova-plugin-network-information.NetworkInfoProxy",
+        "pluginId": "cordova-plugin-network-information",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-customconfigparameters/www/customconfigparameters.js",
+        "id": "cordova-plugin-customconfigparameters.CustomConfigParameters",
+        "pluginId": "cordova-plugin-customconfigparameters",
+        "clobbers": [
+            "CustomConfigParameters"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-appversion/www/app-version.js",
+        "id": "cordova-plugin-appversion.RareloopAppVersion",
+        "pluginId": "cordova-plugin-appversion",
+        "clobbers": [
+            "AppVersion"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -59,7 +98,10 @@ module.exports.metadata =
     "cordova-plugin-whitelist": "1.3.3",
     "onesignal-cordova-plugin": "2.4.6",
     "cordova-plugin-splashscreen": "5.0.2",
-    "cordova-plugin-statusbar": "2.4.2"
+    "cordova-plugin-statusbar": "2.4.2",
+    "cordova-plugin-network-information": "2.0.1",
+    "cordova-plugin-customconfigparameters": "3.0.0",
+    "cordova-plugin-appversion": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
